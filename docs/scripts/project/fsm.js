477b493057217e88fee6013b33f8112a1af67eaf
raassh-23 <muh.nur.abdurrauf@gmail.com>
2023-05-07 09:37:27 +0700

    update preview

--- docs/scripts/project/fsm.js
+++ docs/scripts/project/fsm.js
@@ -5,7 +5,8 @@
  *    [key: string]: {
  *      actions?: {
  *        onEnter?: (() => void),
- *        onExit?: (() => void)
+ *        onExit?: (() => void),
+ *        onTick?: (() => void),
  *      },
  *      transitions: {
  *        [key: string]: {
@@ -16,7 +17,17 @@
  *          },
  *          action?: () => void
  *        }
- *      }
+ *      },
+ *      conditionalTransitions: [
+ *        {
+ *          target: string,
+ *          condition: {
+ *             evaluate: () => boolean,
+ *             onFalse?: () => void
+ *          },
+ *          action?: () => void
+ *        }
+ *      ],
  *    }
  *  }
  * }} StateMachineDefinition
@@ -25,7 +36,9 @@
 /**
  * @typedef {{
  *   value: string,
- *   transition: (event: string) => string
+ *   transition: (event: string) => string,
+ *   updateConditional: () => boolean,
+ *   tick: () => void
  * }} StateMachine
  */
 
@@ -48,29 +61,80 @@ export function createMachine(stateMachineDefinition) {
                 return;
             }
 
-            const condition = destinationTransition.condition?.evaluate() ?? true;
+            const destinationState = changeState(
+                currentStateDefinition, 
+                destinationTransition,
+                stateMachineDefinition,
+            );
 
-            if (!condition) {
-                destinationTransition.condition?.onFalse?.();
+            if (!destinationState) {
                 return;
             }
-
-            const destinationState = destinationTransition.target;
-            const destinationStateDefinition =
-                stateMachineDefinition.states[destinationState];
-
-            destinationTransition.action?.();
-            currentStateDefinition.actions?.onExit?.();
-            destinationStateDefinition.actions?.onEnter?.();
 
             machine.value = destinationState;
 
             return machine.value;
         },
+        updateConditional: function() {
+            const currentStateDefinition = stateMachineDefinition.states[this.value];
+            const destinationTransitions = currentStateDefinition.conditionalTransitions;
+
+            for (const destinationTransition of destinationTransitions) {
+                const destinationState = changeState(
+                    currentStateDefinition,
+                    destinationTransition,
+                    stateMachineDefinition,
+                );
+
+                if (!destinationState) {
+                    continue;
+                }
+
+                machine.value = destinationState;
+                return true;
+            }
+
+            return false;
+        },
+        tick: function() {
+            const currentStateDefinition = stateMachineDefinition.states[this.value];
+            currentStateDefinition.actions?.onTick?.();
+        }
     }
 
     const initialStateDefinition = stateMachineDefinition.states[machine.value];
     initialStateDefinition.actions?.onEnter?.();
 
     return machine;
+}
+
+/**
+ * @param {{
+ *   target: string,
+ *   condition?: {
+ *      evaluate: () => boolean,
+ *      onFalse?: () => void
+ *   },
+ *   action?: () => void
+ * }} destinationTransition 
+ * 
+ * @returns {string | null}
+ */
+function changeState(currentStateDefinition, destinationTransition, stateMachineDefinition) {
+    const condition = destinationTransition.condition?.evaluate() ?? true;
+
+    if (!condition) {
+        destinationTransition.condition.onFalse?.();
+        return null;
+    }
+
+    const destinationState = destinationTransition.target;
+    const destinationStateDefinition =
+        stateMachineDefinition.states[destinationState];
+
+    destinationTransition.action?.();
+    currentStateDefinition.actions?.onExit?.();
+    destinationStateDefinition.actions?.onEnter?.();
+
+    return destinationState;
 }
